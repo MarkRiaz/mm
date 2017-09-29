3ea2caf60bfa21a6bd50a0050ea6152c766475c7
MarkRIaz <markriaz@mail.ru>
2017-09-29 17:11:37 +0200

    d model description

--- db/migrate/20170928094036_create_descriptions.rb
+++
@@ -1,9 +0,0 @@
-class CreateDescriptions < ActiveRecord::Migration[5.1]
-  def change
-    create_table :descriptions do |t|
-      t.references :admin, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
